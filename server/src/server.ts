304fb036eb82f18c340bc2b5244147e88e8c3627
williamstyronejr <itenory@gmail.com>
2021-08-13 11:04:22 -0400

    Linting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,19 @@
-import { createServer } from "http";
-import app from "./services/app";
-import { setUpSocket, closeSocketServer } from "./services/socket";
-import { setupRedis, closeRedis } from "./services/redis";
-import logger from "./services/logger";
+import { createServer } from 'http';
+import app from './services/app';
+import { setUpSocket } from './services/socket';
+import { setupRedis } from './services/redis';
+import logger from './services/logger';
 
-const { REDIS_HOST, REDIS_PORT } = process.env;
+const { REDIS_HOST, REDIS_PORT, REDIS_URL } = process.env;
 
 exports.startServer = async (IP: string, PORT: number) => {
   const httpServer = createServer(app);
   setUpSocket(httpServer);
-  await setupRedis(REDIS_HOST, REDIS_PORT ? parseInt(REDIS_PORT) : undefined);
+  await setupRedis(
+    REDIS_HOST,
+    REDIS_PORT ? parseInt(REDIS_PORT, 10) : undefined,
+    REDIS_URL,
+  );
 
   httpServer.listen(PORT, IP, () => {
     logger.info(`Server running on ${IP}:${PORT}`);
