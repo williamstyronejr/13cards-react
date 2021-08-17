2d6afc100514064eca159d6fdb9d5b3bdc40ca9c
williamstyronejr <itenory@gmail.com>
2021-08-17 16:18:32 -0400

    Imports global css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Root from './Root';
+import './global.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
