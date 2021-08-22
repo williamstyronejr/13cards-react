69f1fed583d5a5c9be7ff0cce8e91b78b43a09cb
williamstyronejr <itenory@gmail.com>
2021-08-22 19:30:55 -0400

    Create card compare func

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -12,3 +12,32 @@ export function generateString(length = 16, append = ''): string {
     ).substring(0, length) + append
   );
 }
+
+/**
+ * Compares two cards to determine the smaller card with rules of the game.
+ * @param c1 Card value in "suitnumber" format
+ * @param c2 Card value in "suitnumber" format
+ * @return {Number} Returns a number indicating the comparison of the two cards.
+ *  0 if equal, 1 if c1 is greater then c2, or -1 if c1 is less than c2.
+ */
+export function compareCards(c1: string, c2: string): number {
+  if (c1 === c2) return 0;
+
+  const c1Suit = c1.charAt(0);
+  const c2Suit = c2.charAt(0);
+  let c1Value = parseInt(c1.substr(1), 10);
+  let c2Value = parseInt(c2.substr(1), 10);
+  c1Value = c1Value < 3 ? c1Value + 20 : c1Value;
+  c2Value = c2Value < 3 ? c2Value + 20 : c2Value;
+
+  // Same card value
+  if (c1Value === c2Value) {
+    if (c1Suit === 'h') return 1;
+    if (c1Suit === 's') return -1;
+    if (c1Suit === 'c' && (c2Suit === 'd' || c2Suit === 'h')) return -1;
+    if (c1Suit === 'd' && c2Suit === 'h') return -1;
+    return 1;
+  }
+
+  return c1Value > c2Value ? 1 : -1;
+}
